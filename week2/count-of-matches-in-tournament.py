8a477d5608a2b40eeef91a891d6e386c5e2f39af
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2023-12-09 06:58:06 +0300

    Added solution for Count of Matches in Tournament

---
+++ week2/count-of-matches-in-tournament.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        matches = 0
+        while n > 1:
+            matches += n // 2
+            n = (n + 1) // 2
+        return matches
