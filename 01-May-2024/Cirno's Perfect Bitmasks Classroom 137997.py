b42956c734827c2c2cbea5b5030b42ce1b46a25b
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-01 10:36:46 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 137997.py
@@ -0,0 +1,25 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+import sys
+t = int(input())
+
+def input():
+    return sys.stdin.readline().strip()
+
+for _ in range(t):
+    
+    nu = int(input())
+    if nu == 1:
+        print(3)
+        continue
+    i = 1
+    flag =False
+    while i<nu:
+        if i^nu and i&nu:
+            print(i)
+            flag = True
+            break
+        i<<=1
+        
+    if flag == False:
+        print(nu|1)
