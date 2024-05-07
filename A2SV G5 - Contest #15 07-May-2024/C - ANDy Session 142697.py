1cd3f59d076df3c1a20dccbe5ab10f5f6fece1d7
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-07 20:29:45 +0300

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/C - ANDy Session 142697.py
@@ -0,0 +1,28 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+def maximize_AND(n, k, a):
+    sums = 0
+    for i in range(30,-1,-1):
+        po = pow(2,i)
+        
+        p = 0
+        for j in range(n):
+            val = po&a[j]
+
+            if val == 0:
+                p+=1
+                
+        if p<=k:
+            k -= p
+            sums += po
+            
+    return sums
+
+t = int(input())
+
+
+for _ in range(t):
+
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    print(maximize_AND(n, k, a))
