53fd5cf52d22725f97a9589769ee286551c9816b
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-07 20:29:48 +0300

    Created 'B - tourist orz!' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/B - tourist orz! 142698.py
@@ -0,0 +1,34 @@
+# Problem: B - tourist orz! - https://codeforces.com/gym/522079/problem/B
+
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right
+import math
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+
+
+def solve():
+    n,z = map(int,input().split())
+    a= IL()
+    
+    maxval = float("-inf")
+    
+    for i in range(n):
+        orr = a[i]|z
+        anr = a[i]&z
+        mx = max(orr,anr)
+        
+        maxval = max(maxval,mx)
+    return maxval
+
+# t = 1
+t = int(input())
+for _ in range(t):
+    print(solve())
+
+
