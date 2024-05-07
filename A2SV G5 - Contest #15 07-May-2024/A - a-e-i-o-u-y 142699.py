9e5a90375ce83b65415065b6aeea410ec0445a7c
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-07 20:29:54 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/A - a-e-i-o-u-y 142699.py
@@ -0,0 +1,37 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right
+import math
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+
+
+def solve():
+    vowels = ("a", "e", "i", "o", "u", "y")
+    
+    n = int(input()) 
+    s = input().strip()  
+    
+    wr = []
+    for i in range(n):
+        wr.append(s[i])
+        
+        if i>0 and s[i] in vowels and s[i-1] in vowels:
+            wr.pop()
+            
+            
+    return ''.join(wr)
+            
+
+t = 1
+# t = int(input())
+for _ in range(t):
+    print(solve())
+
+
