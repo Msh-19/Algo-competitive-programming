785bddb1a90b25e775a8f0561704f2064a18058a
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-14 18:16:23 +0300

    Created 'B - Instructions' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/B - Instructions 149919.py
@@ -0,0 +1,28 @@
+# Problem: B - Instructions - https://codeforces.com/gym/523525/problem/B
+
+from bisect import bisect_left, bisect_right
+import math
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+
+
+def solve():
+    n = I()
+    li = IL()
+    dp = [0]*n
+    for i in range(n-1,-1,-1):
+        if i+li[i] < n:
+            dp[i] = li[i] + dp[i+li[i]]
+        else:
+            dp[i] = li[i]
+    
+    return max(dp)
+# t = 1
+t = int(input())
+for _ in range(t):
+    print(solve())
