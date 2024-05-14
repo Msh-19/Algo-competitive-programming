14a482d2f305d3bd3de3eba1d3638a77debf24da
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-14 18:16:32 +0300

    Created 'A - Non-Identity Permutations' by A2SV Hub

---
+++ A2SV G5 - Contest #16 14-May-2024/A - Non-Identity Permutations 149922.py
@@ -0,0 +1,32 @@
+# Problem: A - Non-Identity Permutations - https://codeforces.com/gym/523525/problem/A
+
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right
+import math
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+
+t = int(input())
+for _ in range(t):
+
+    n = I()
+    li = []
+    res = []
+    for i in range(n):
+        li.append(i+1)
+    
+
+    for j in range(n):
+        res.append(li[j-1])
+
+    print(*res)
+        
+
+
+
+
