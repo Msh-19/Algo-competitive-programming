35f930950dc13c88a2973fd36f0203fa33698c78
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-14 15:46:16 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 14-May-2024/Complicated GCD 149244.py
@@ -0,0 +1,13 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+import sys
+from math import gcd
+def input():
+    return sys.stdin.readline().strip()
+
+a, b = input().split()
+
+if a == b:
+    print(a)
+else:
+    print(1)
