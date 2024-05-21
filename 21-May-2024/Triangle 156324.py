018babbf4c89b2adeb4c578bfffee44ab3e32962
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-21 15:55:41 +0300

    Created 'Triangle' by A2SV Hub

---
+++ 21-May-2024/Triangle 156324.py
@@ -0,0 +1,10 @@
+# Problem: Triangle - https://leetcode.com/problems/triangle/
+
+class Solution(object):
+    def minimumTotal(self, triangle):
+        dp = [0]*(len(triangle)+1)
+
+        for row in triangle[::-1]:
+            for i,n in enumerate(row):
+                dp[i] =  n + min(dp[i],dp[i+1])
+        return dp[0]
