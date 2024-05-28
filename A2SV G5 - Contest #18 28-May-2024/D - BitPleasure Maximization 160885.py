3a2f9ff0048cb4b1ea2ac1ee4379c6648e004dd9
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-05-28 13:40:40 +0300

    Created 'D - BitPleasure Maximization' by A2SV Hub

---
+++ A2SV G5 - Contest #18 28-May-2024/D - BitPleasure Maximization 160885.py
@@ -0,0 +1,55 @@
+# Problem: D - BitPleasure Maximization - https://codeforces.com/gym/526229/problem/D
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None,None]
+
+class Trie:
+    def __init__(self,val):
+        self.root = TrieNode()
+        self.bit_len = val
+
+
+    def insert(self,num):
+        cur = self.root
+     
+        for  i in range(self.bit_len, -1, -1):
+            bit = 1 if num & (1 << i)else 0
+            if cur.children[bit] is None:
+                cur.children[bit] = TrieNode()
+            cur = cur.children[bit]
+    
+    def findMax(self, num):
+        cur = self.root
+        max_num = 0
+        for i in range(self.bit_len, -1,-1):
+            bit= 1 if num & (1<<i) else 0 
+            if cur.children[1-bit] is not None:
+                max_num = max_num | (1<<i)
+                cur = cur.children[1-bit]
+            else:
+                cur = cur.children[bit]
+        return max_num
+import sys
+n = int(sys.stdin.readline().strip())
+
+nums = list(map(int, sys.stdin.readline().strip().split()))
+
+trie = Trie(len(bin(max(nums))))
+
+tot_pref = 0
+nums = [0] + nums
+
+for num in nums:
+    tot_pref ^= num
+
+cur_pref = 0
+ans = 0
+
+for  num in nums:
+    cur_pref ^= num
+    trie.insert(cur_pref)
+    ans = max(ans, trie.findMax(cur_pref ^ tot_pref))
+
+print(ans)
+    
