d06ec494213b79b33282e31514b47991681d457c
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-06-14 22:43:39 +0300

    Created 'Minimum Increment to Make Array Unique' by A2SV Hub

---
+++ 14-Jun-2024/Minimum Increment to Make Array Unique 168460.py
@@ -0,0 +1,13 @@
+# Problem: Minimum Increment to Make Array Unique - https://leetcode.com/problems/minimum-increment-to-make-array-unique/
+
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+
+        for i in range(1,len(nums)):
+            if nums[i-1] >= nums[i]:
+                res += 1 + nums[i-1] - nums[i]
+                nums[i] = nums[i-1] + 1
+
+        return res
