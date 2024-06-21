ccab9184f7790d7baa4efd823ee67972cf8a0b49
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-06-21 21:16:19 +0300

    Created 'Anji's Binary Tree' by A2SV Hub

---
+++ 21-Jun-2024/Anji's Binary Tree 171166.py
@@ -0,0 +1,37 @@
+# Problem: Anji's Binary Tree - https://codeforces.com/contest/1900/problem/C
+
+from collections import defaultdict
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+    graph = defaultdict(list)
+    
+    for i in range(1,n+1):
+        graph[i].append(s[i-1])
+        a = list(map(int,input().split()))
+        for num in a:
+            graph[i].append(num)
+            
+    stack = []
+    stack.append(1)
+    ans = []
+    dic_ = defaultdict(int)
+    
+    while stack:
+        curr = stack.pop()
+        if graph[curr][1] == 0 and graph[curr][2] ==0:
+            ans.append(dic_[curr])
+        for idx,neigh in enumerate(graph[curr]):
+            add = 0
+            if neigh!=0 and idx!=0:
+                if graph[curr][0]!="L" and idx == 1:
+                    add+=1
+                if graph[curr][0]!="R" and idx == 2:
+                    add+=1
+                stack.append(neigh)
+                dic_[neigh] = dic_[curr]+add
+    print(min(ans))
+    
