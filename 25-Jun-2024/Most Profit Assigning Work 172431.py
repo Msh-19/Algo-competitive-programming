41aec463d1f68801a368015086fbbee379f17da3
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-06-25 15:06:42 +0300

    Created 'Most Profit Assigning Work' by A2SV Hub

---
+++ 25-Jun-2024/Most Profit Assigning Work 172431.py
@@ -0,0 +1,34 @@
+# Problem: Most Profit Assigning Work - https://leetcode.com/problems/most-profit-assigning-work/
+
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        job_profile = [(0,0)]
+        for i in range(len(difficulty)):
+            job_profile.append((difficulty[i],profit[i]))
+
+
+        job_profile.sort()
+        for i in range(len(job_profile) - 1):
+            job_profile[i + 1] = (
+                job_profile[i + 1][0],
+                max(job_profile[i][1], job_profile[i + 1][1]),
+            )
+        net_profit = 0
+        for i in range(len(worker)):
+            ability = worker[i]
+
+            # Find the job with just smaller or equal difficulty than ability.
+
+            l, r = 0, len(job_profile) - 1
+            job_profit = 0
+            while l <= r:
+                mid = (l + r) // 2
+                if job_profile[mid][0] <= ability:
+                    job_profit = max(job_profit, job_profile[mid][1])
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # Increment profit of current worker to total profit.
+
+            net_profit += job_profit
+        return net_profit
