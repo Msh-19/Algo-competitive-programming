db3efabc4d12ad67d0b4e337aef0b4f85fb7c34d
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-02 23:08:40 +0300

    Created 'Number of Wonderful Substrings' by A2SV Hub

---
+++ 02-Jul-2024/Number of Wonderful Substrings 175809.py
@@ -0,0 +1,26 @@
+# Problem: Number of Wonderful Substrings - https://leetcode.com/problems/number-of-wonderful-substrings/
+
+class Solution:
+    def wonderfulSubstrings(self, word: str) -> int:
+        freq = {}
+
+        freq[0] = 1
+
+        mask = 0
+        res = 0
+        for c in word:
+            bit = ord(c) - 97
+
+            mask ^= (1 << bit)
+
+            if mask in freq:
+                res += freq[mask]
+                freq[mask] += 1
+            else:
+                freq[mask] = 1
+
+            for odd_c in range(0, 10):
+                if (mask ^ (1 << odd_c)) in freq:
+                    res += freq[mask ^ (1 << odd_c)]
+
+        return res
