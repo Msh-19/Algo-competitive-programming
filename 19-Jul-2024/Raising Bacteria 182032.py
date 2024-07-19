57731492d8ed85686a1d46d015189633c4234c6a
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-19 23:37:43 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 19-Jul-2024/Raising Bacteria 182032.py
@@ -0,0 +1,16 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+n = int(input())
+
+count = 0
+
+if n%2 !=0:
+    count +=1
+if n == 1:
+    print(1)
+else:
+    while n!=1:
+        n = n//2
+        if n%2 != 0:
+            count += 1
+    print(count)
