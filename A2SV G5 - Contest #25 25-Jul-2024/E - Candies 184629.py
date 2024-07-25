297a8bbba8c5ce2bf8f5f2678cdc68bab960f214
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:38:57 +0300

    Created 'E - Candies' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/E - Candies 184629.py
@@ -0,0 +1,28 @@
+# Problem: E - Candies - https://codeforces.com/gym/535749/problem/E
+
+Max = 10**5 +1
+Mod = 10**9 +7
+
+t,k = map(int, input().split())
+
+prfx = [1] * Max
+prfx[0] = 0
+
+prfx[k] = 2
+
+
+for i in range(k+1,Max):
+    prfx[i] = (prfx[i-1] + prfx[i-k])%Mod
+    
+    
+for i in range(k+1,Max):
+    prfx[i] = (prfx[i-1] + prfx[i - k]) % Mod
+    
+    
+for i in range(1,Max):
+    prfx[i] = (prfx[i] + prfx[i-1]) % Mod
+    
+for _ in range(t):
+    a,b = map(int,input().split())
+    
+    print((prfx[b] - prfx[a-1]) % (10**9 + 7))
