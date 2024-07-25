5eaf85555f5c5b6606893f53c851dc830de6919a
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:42:08 +0300

    Created 'B - Your Hackathon Project - a Game' by A2SV Hub

---
+++ A2SV G5 - Contest #24 25-Jul-2024/B - Your Hackathon Project - a Game 184635.py
@@ -0,0 +1,20 @@
+# Problem: B - Your Hackathon Project - a Game - https://codeforces.com/gym/534160/problem/B
+
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+
+l = [0] + [max(0,li[i] - li[i+1]) for i in range(n-1)]
+r = [0] + [max(0,li[i] - li[i-1]) for i in range(1,n)]
+
+for i in range(n-1):
+    l[i+1] += l[i]
+    r[i+1] += r[i]
+
+for _ in range(m):
+    s,t = map(int, input().split())
+    
+    if s<t:
+        print(l[t-1] - l[s-1])
+        
+    else:
+        print(r[s-1] - r[t-1])
