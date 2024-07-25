8caba4c7dda51f301ba15059d935d8bf5d881f01
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:38:30 +0300

    Created 'B - Optimal Point' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/B - Optimal Point 184625.py
@@ -0,0 +1,31 @@
+# Problem: B - Optimal Point - https://codeforces.com/gym/535749/problem/B
+
+
+import sys
+
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+
+def solve():
+    n,k = map(int, input().split())
+    
+    fron = False
+    back = False
+    for _ in range(n):
+        s,e = map(int,input().split())
+        if s==k:
+            fron = True
+        if e == k:
+            back = True
+    if fron and back:
+        return "YES"
+    else:
+        return "NO"
+
+# t = 1
+t = int(input())
+for _ in range(t):
+    print(solve())
