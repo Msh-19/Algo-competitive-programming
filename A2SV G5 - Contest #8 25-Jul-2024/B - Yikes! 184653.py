4b8766f7686e7b31b9e79bde8813e01b63be8a1a
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:48:39 +0300

    Created 'B - Yikes!' by A2SV Hub

---
+++ A2SV G5 - Contest #8 25-Jul-2024/B - Yikes! 184653.py
@@ -0,0 +1,16 @@
+# Problem: B - Yikes! - https://codeforces.com/gym/511242/problem/B
+
+import bisect
+m = int(input())
+pi = list(map(int,input().split()))
+n = int(input())
+pi2 = list(map(int,input().split()))
+cur = 0
+pre = []
+pre.append(0)
+for i in range(len(pi)):
+    cur+=pi[i]
+    pre.append(cur)
+for j in range(len(pi2)):
+    print(bisect.bisect_left(pre, pi2[j]))
+    
