b896ff81219940d8f95d7a5721f316202db254ca
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:38:18 +0300

    Created 'C - Selecting Theatre Troupe' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/C - Selecting Theatre Troupe 184624.py
@@ -0,0 +1,13 @@
+# Problem: C - Selecting Theatre Troupe - https://codeforces.com/gym/535749/problem/C
+
+from math import comb as c
+
+bo,gi,t = map(int, input().split())
+
+ans = 0
+
+for i in range(4,t):
+    if bo>= i and gi >= t - i:
+        ans+= c(bo,i)*c(gi,t-i)
+
+print(ans)
