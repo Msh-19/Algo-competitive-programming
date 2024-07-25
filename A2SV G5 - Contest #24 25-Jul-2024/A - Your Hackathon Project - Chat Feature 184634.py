5e6b2f047fe0da9ef51d0e39d33d9831eb24d910
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:41:57 +0300

    Created 'A - Your Hackathon Project - Chat Feature' by A2SV Hub

---
+++ A2SV G5 - Contest #24 25-Jul-2024/A - Your Hackathon Project - Chat Feature 184634.py
@@ -0,0 +1,19 @@
+# Problem: A - Your Hackathon Project - Chat Feature - https://codeforces.com/gym/534160/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    li = input()
+
+    count = 0
+    for i in range(n-1,-1,-1):
+        if li[i] == ")":
+            count +=1 
+        else:
+            break
+        
+    if count > n//2:
+        print("Yes")
+    else:
+        print("No")
