24e2c02bc33d1bab0fb6114a06f4355774faf938
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:44:55 +0300

    Created 'B - Distinct Digits' by A2SV Hub

---
+++ A2SV G5 - Contest #21 25-Jul-2024/B - Distinct Digits 184640.py
@@ -0,0 +1,16 @@
+# Problem: B - Distinct Digits - https://codeforces.com/gym/530187/problem/B
+
+t = int(input())
+for _ in range(t):
+    nums = []
+    num = int(input())
+    
+    for digit in range(9,0,-1):
+        if num >=digit:
+            nums.append(digit)
+            num -= digit
+            
+    nums.sort()
+    
+    res = ''.join(map(str,nums))
+    print(res)
