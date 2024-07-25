9dc5fad2b3515b5d17300f6e07434f0da93404ee
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:35:34 +0300

    Created 'A - Se7en' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/A - Se7en 184619.py
@@ -0,0 +1,12 @@
+# Problem: A - Se7en - https://codeforces.com/gym/537362/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    num = int(input())
+    k = num%7
+    
+    if (num%10) < k:
+        print(num-k+7)
+    else:
+        print(num-k)
