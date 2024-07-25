534c58c977c060ac52717bd49364fcaf23c9283b
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:46:42 +0300

    Created 'A - Duale String' by A2SV Hub

---
+++ A2SV G5 - Contest #14 25-Jul-2024/A - Duale String 184643.py
@@ -0,0 +1,27 @@
+# Problem: A - Duale String - https://codeforces.com/gym/520688/problem/A
+
+from collections import defaultdict, Counter, deque
+import sys
+ 
+def input(): return sys.stdin.readline().strip()
+def I(): return int(input())
+def IL(): return list(map(int, input().split()))
+def S(): return input()
+def SL(): return input().split()
+ 
+ 
+ 
+def solve():
+    s = list(input())
+    if len(s)%2 != 0:
+        return "NO"
+    mid = len(s)//2 
+    if s[:mid] == s[mid:]:
+        return "YES"
+    else:
+        return "NO"
+        
+# t = 1
+t = int(input())
+for _ in range(t):
+    print(solve())
