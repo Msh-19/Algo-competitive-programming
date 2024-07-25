9a1c701671c36aed8c978c1dc77dde48c1b2be4f
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:42:16 +0300

    Created 'C - Removal Hack' by A2SV Hub

---
+++ A2SV G5 - Contest #24 25-Jul-2024/C - Removal Hack 184636.py
@@ -0,0 +1,41 @@
+# Problem: C - Removal Hack - https://codeforces.com/gym/534160/problem/C
+
+from collections import defaultdict
+from sys import stdin
+
+
+def input(): return stdin.readline().strip()
+
+N = int(input())
+
+child = defaultdict(list)
+respect = [0]*N
+
+root = 0
+
+for ch in range(N):
+    p, c = map(int, input().split())
+    p -= 1
+    respect[ch] = c
+    if p == -2:
+        root = ch
+        continue
+    child[p].append(ch)
+
+stack = [root]
+
+ans = []
+
+while stack:
+    v = stack.pop()
+    ok = respect[v]
+    for ch in child[v]:
+        stack.append(ch)
+        if respect[ch] == 0:
+            ok = False
+    if ok:
+        ans.append(v + 1)
+if not ans:
+    print(-1)
+else:
+    print(*sorted(ans))
