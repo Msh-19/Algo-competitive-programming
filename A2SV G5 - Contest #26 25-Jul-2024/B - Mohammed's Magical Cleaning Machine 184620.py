e1c1ca42865f292d7a02af45dae9ec622fb9823a
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:35:47 +0300

    Created 'B - Mohammed's Magical Cleaning Machine' by A2SV Hub

---
+++ A2SV G5 - Contest #26 25-Jul-2024/B - Mohammed's Magical Cleaning Machine 184620.py
@@ -0,0 +1,15 @@
+# Problem: B - Mohammed's Magical Cleaning Machine - https://codeforces.com/gym/537362/problem/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    li = list(map(int, input().split()))
+    count = 0
+    
+    for i in range(len(li)-1):
+        if li[i] != 0:
+            count += li[i]
+        elif count != 0 and li[i] == 0:
+            count += 1
+    print(count)
