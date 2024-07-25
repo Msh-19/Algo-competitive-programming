698b5ddce0ca3d6a3dd34acb13b370423027be34
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-07-25 20:38:12 +0300

    Created 'A - A2SV Contest #25' by A2SV Hub

---
+++ A2SV G5 - Contest #25 25-Jul-2024/A - A2SV Contest #25 184623.py
@@ -0,0 +1,14 @@
+# Problem: A - A2SV Contest #25 - https://codeforces.com/gym/535749/problem/A
+
+pv,pm,vt,mt = (map(int, input().split()))
+
+v = max((3*pv)/10,pv - (pv/250)*vt)
+m = max((3*pm)/10,pm - (pm/250)*mt)
+
+
+if v > m:
+    print("Misha")
+elif v < m:
+    print("Vasya")
+else:
+    print("Tie")
