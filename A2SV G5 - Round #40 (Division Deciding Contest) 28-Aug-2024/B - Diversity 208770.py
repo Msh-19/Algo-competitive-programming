ae7db55a89142c5b46d64cc90b699bd1d1ca9fd2
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-08-28 23:14:24 +0300

    Created 'B - Diversity' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/B - Diversity 208770.py
@@ -0,0 +1,18 @@
+# Problem: B - Diversity - https://codeforces.com/gym/543431/problem/B
+
+from collections import Counter
+li = input()
+
+count = Counter(li)
+t = int(input())
+
+
+res = t-len(count)
+if len(li) < t:
+    print("impossible")
+else:
+    if res < 0:
+        print(0)
+    else:
+        print(res)
+
