37979c3c2d2d0f6ef814b9ee8bbd94cd5a980eaa
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-08-28 23:17:07 +0300

    Created 'A - Collecting Coins' by A2SV Hub

---
+++ A2SV G5 - Contest #28 28-Aug-2024/A - Collecting Coins 208773.py
@@ -0,0 +1,18 @@
+# Problem: A - Collecting Coins - https://codeforces.com/gym/540354/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    a,b,c,n = map(int, input().split())
+    
+    val = max(a,b,c)
+    
+    d = abs(val - a) + abs(val - b) + abs(val-c)
+    
+    n= n - d
+    if n < 0:
+        print("NO")
+    elif n%3 == 0:
+        print("YES")
+    else:
+        print("NO")
