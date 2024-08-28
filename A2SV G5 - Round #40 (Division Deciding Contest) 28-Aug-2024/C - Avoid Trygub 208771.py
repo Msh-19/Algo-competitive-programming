fbca9bca92fabaa7a277643917fc33ca04cd7261
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-08-28 23:14:30 +0300

    Created 'C - Avoid Trygub' by A2SV Hub

---
+++ A2SV G5 - Round #40 (Division Deciding Contest) 28-Aug-2024/C - Avoid Trygub 208771.py
@@ -0,0 +1,10 @@
+# Problem: C - Avoid Trygub - https://codeforces.com/gym/543431/problem/C
+
+t = int(input())
+target = "trygub"
+    
+for _ in range(t):
+    n = int(input())
+    li = (input())
+    print( "".join(sorted(li)))
+    
