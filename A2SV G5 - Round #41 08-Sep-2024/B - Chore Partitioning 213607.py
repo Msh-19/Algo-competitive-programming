c13dcf890cd48a5c3c776fb5ae4904a16a3f7191
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-09-08 22:23:00 +0300

    Created 'B - Chore Partitioning' by A2SV Hub

---
+++ A2SV G5 - Round #41 08-Sep-2024/B - Chore Partitioning 213607.py
@@ -0,0 +1,12 @@
+# Problem: B - Chore Partitioning - https://codeforces.com/gym/545013/problem/B
+
+n,a,b = map(int,input().split())
+
+li = list(map(int, input().split()))
+li.sort()
+
+if li[b-1] == li[b]:
+    print(0)
+    
+else:
+    print((li[b] - li[b-1]))
