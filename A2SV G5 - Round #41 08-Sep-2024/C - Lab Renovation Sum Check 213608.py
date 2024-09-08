a07ede204c01de072ee5822268cb45aaba52c58b
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-09-08 22:23:09 +0300

    Created 'C - Lab Renovation Sum Check' by A2SV Hub

---
+++ A2SV G5 - Round #41 08-Sep-2024/C - Lab Renovation Sum Check 213608.py
@@ -0,0 +1,22 @@
+# Problem: C - Lab Renovation Sum Check - https://codeforces.com/gym/545013/problem/C
+
+t = int(input())
+mat = []
+for _ in range(t):
+    mat.append(list(map(int,input().split())))
+
+flag = True
+
+for r in range(t):
+    for c in range(t):
+        if mat[r][c]!= 1:
+            sums = []
+            for i1 in range(t):
+                for j1 in range(t):
+                    sums.append(mat[i1][c] + mat[r][j1])
+            if mat[r][c] not in sums:
+                flag = False
+if flag:
+    print("Yes")
+else:
+    print("No")
