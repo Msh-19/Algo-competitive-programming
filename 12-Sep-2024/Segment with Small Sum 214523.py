24cd4dd72250f2729e8c0c83a26d4368af83bd3b
Mohammed Shemim <110376160+Msh-19@users.noreply.github.com>
2024-09-12 15:13:01 +0300

    Created 'Segment with Small Sum' by A2SV Hub

---
+++ 12-Sep-2024/Segment with Small Sum 214523.py
@@ -0,0 +1,21 @@
+# Problem: Segment with Small Sum - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+n,s = map(int,input().split())
+
+li = list(map(int, input().split()))
+maxSum = 0
+
+l = 0
+curr_sum = 0
+max_len = 0
+
+for r in range(len(li)):
+    curr_sum += li[r]
+    
+    while curr_sum > s and l <=r:
+        curr_sum -= li[l]
+        l+=1
+        
+    max_len = max(max_len,r - l + 1)
+
+print(max_len)
